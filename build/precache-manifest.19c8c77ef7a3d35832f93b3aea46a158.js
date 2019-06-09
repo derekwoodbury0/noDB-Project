1dc4ff04bdffc2ee4331e6fcfead8f310f8f99c3
Derek <Derek@Dereks-MBP.lan>
2019-06-09 10:55:11 -0600

    update

---
+++ build/precache-manifest.19c8c77ef7a3d35832f93b3aea46a158.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4748cc1aa3aca859a46f91b6a0bc4aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14bbdc1026ccdf08951d",
+    "url": "/static/css/main.c3f0d0c1.chunk.css"
+  },
+  {
+    "revision": "393348adc838fb57772f",
+    "url": "/static/js/2.0ac49772.chunk.js"
+  },
+  {
+    "revision": "14bbdc1026ccdf08951d",
+    "url": "/static/js/main.49b36c71.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
